0af4ea55fa0ee85facaac2bfa9e5fb32e21e4d05
EnzoWay <vincenzo.cesarano92@gmail.com>
2024-06-30 18:32:26 +0200

    up

--- FRONT-END/UNIT1/Settimana3/D4-Esercizio/assets/javascript/script.js
+++ FRONT-END/UNIT1/Settimana3/D4-Esercizio/assets/javascript/script.js
@@ -1,15 +1,19 @@
 const tabellone = document.getElementById('tabellone');
 const btnRandomNumber = document.getElementById('btnRandomNumber');
+const ultimoEstratto = document.getElementById('ultimoEstratto');
+const cartelle = document.getElementById('cartelle');
+const btnBuy = document.getElementById('btnBuy');
+const buyCartelle = document.getElementById('buyCartelle');
 
-
+const panariello = [];
 const totalTabellone = [];
+const numberCartelle = [];
 
 function numTabellone() {
     for (let i = 1; i < 91; i++) {
         totalTabellone.push(i);
         const divNum = document.createElement('div');
         divNum.classList.add('divNumeri');
-        divNum.setAttribute('id', `num${i}`);
         const h4Num = document.createElement('h4');
         h4Num.innerText = [i];
         tabellone.appendChild(divNum);
@@ -20,29 +24,67 @@ function numTabellone() {
 numTabellone();
 
 
-
-
-btnRandomNumber.addEventListener('click', function () {
+btnRandomNumber.addEventListener('click', function (e) {
+    e.preventDefault();
     randomNumber();
-    
+    equal();
 })
 
 
-const panariello = [];
-
 function randomNumber() {
-    let random1 = Math.floor(Math.random() * 89 + 1);
+    let random1 = Math.floor(Math.random() * 90 + 1);
 
     if (!panariello.includes(random1)) {
         panariello.push(random1);
+        ultimoEstratto.innerText = `${random1}`;
     } else {
         randomNumber();
     };
 }
 
 
-function equal() {
-    if(panariello.includes(totalTabellone)){
-        
+
+
+btnBuy.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    numCartelle(parseInt(buyCartelle.value))
+})
+
+function numCartelle(num) {
+    for (let i = 0; i < num; i++) {
+        const divCartella = document.createElement('div');
+        divCartella.classList.add('cartella');
+
+
+        const currentCartellaNumbers = [];
+        while (currentCartellaNumbers.length < 15) {
+            const num = Math.floor(Math.random() * 90) + 1;
+            if (!currentCartellaNumbers.includes(num)) {
+                currentCartellaNumbers.push(num);
+            }
+        }
+
+        currentCartellaNumbers.forEach(num => {
+            const divNum = document.createElement('div');
+            divNum.classList.add('divNumeri');
+            const h4Num = document.createElement('h4');
+            h4Num.innerText = num;
+            divCartella.appendChild(divNum);
+            divNum.appendChild(h4Num);
+        });
+        cartelle.appendChild(divCartella);
     }
+}
+
+
+
+
+function equal() {
+    const boh = document.querySelectorAll('.divNumeri');
+    boh.forEach(item => {
+        if (panariello.includes(parseInt(item.textContent))) {
+            item.classList.add('divEstratti');
+        }
+    });
 }
