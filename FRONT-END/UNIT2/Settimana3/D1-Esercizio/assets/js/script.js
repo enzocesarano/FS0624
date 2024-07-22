d8b5a38f237252367ae1cee5224a38f93d67f520
EnzoWay <vincenzo.cesarano92@gmail.com>
2024-07-22 16:23:18 +0200

    Settimana 3 D1

---
+++ FRONT-END/UNIT2/Settimana3/D1-Esercizio/assets/js/script.js
@@ -0,0 +1,107 @@
+// PRIMO ESERCIZIO
+
+class User {
+    constructor(_firstName, _lastName, _age, _location) {
+        this.firstName = _firstName
+        this.lastName = _lastName
+        this.age = _age
+        this.location = _location
+    }
+
+    compara(utenti) {
+        if (this.age > utenti.age) {
+            return `${this.firstName} è più grande di ${utenti.firstName}`
+        } else if (this.age < utenti.age) {
+            return `${utenti.firstName} è più grande di ${this.firstName}`
+        } else {
+            return `${this.firstName} e ${utenti.firstName} hanno la stessa età`
+        }
+    }
+
+}
+
+const utente1 = new User('Vincenzo', 'Cesarano', 32, 'Napoli')
+const utente2 = new User('Mario', 'Rossi', 52, 'Roma')
+
+console.log(utente1.compara(utente2))
+
+
+
+// SECONDO ESERCIZIO
+
+
+
+class Pet {
+    constructor(_petName, _ownerName, _species, _breed) {
+        this.petName = _petName
+        this.ownerName = _ownerName
+        this.species = _species
+        this.breed = _breed
+    }
+
+    comparaOwner(pets) {
+        if (this.ownerName = pets.ownerName) {
+            console.log('true')
+        }
+    }
+}
+
+
+const form = document.getElementById('form')
+
+const pets = [];
+
+form.addEventListener('submit', function (e) {
+    e.preventDefault()
+
+    const inputName = document.getElementById('petName').value
+    const inputOwner = document.getElementById('ownerName').value
+    const inputSpecies = document.getElementById('speciesName').value
+    const inputBreed = document.getElementById('breedName').value
+
+    const pet = new Pet(inputName, inputOwner, inputSpecies, inputBreed)
+    pets.push(pet)
+    
+    form.reset()
+
+    const list = document.getElementById('list')
+    list.classList.add('row', 'row-cols-5')
+
+    const colonna = document.createElement('div')
+    colonna.classList.add('col', 'd-flex', 'justify-content-center', 'mb-4')
+
+    const card = document.createElement('div')
+    card.classList.add('card')
+    card.style.width = '18rem'
+
+    const cardBody = document.createElement('div')
+    cardBody.classList.add('card-body')
+
+    const namePet = document.createElement('p')
+    namePet.classList.add('d-flex', 'justify-content-between', 'align-items-baseline')
+    namePet.innerHTML = `Name: <span>${inputName}</span>`
+
+    const ownerPet = document.createElement('p')
+    ownerPet.classList.add('d-flex', 'justify-content-between', 'align-items-baseline')
+    ownerPet.innerHTML = `Owner: <span>${inputOwner}</span>`
+
+    const speciesPet = document.createElement('p')
+    speciesPet.classList.add('d-flex', 'justify-content-between', 'align-items-baseline')
+    speciesPet.innerHTML = `Species: <span>${inputSpecies}</span>`
+
+    const breedPet = document.createElement('p')
+    breedPet.classList.add('d-flex', 'justify-content-between', 'align-items-baseline')
+    breedPet.innerHTML = `Breed: <span>${inputBreed}</span>`
+
+
+    list.appendChild(colonna)
+    colonna.appendChild(card)
+    card.appendChild(cardBody)
+    cardBody.appendChild(namePet)
+    cardBody.appendChild(ownerPet)
+    cardBody.appendChild(speciesPet)
+    cardBody.appendChild(breedPet)
+
+})
+
+console.log(pets)
