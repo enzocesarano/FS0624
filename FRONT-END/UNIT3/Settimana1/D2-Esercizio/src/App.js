85d3a8530620b5299c693e650271f4645d7ff9e4
Vincenzo Cesarano <vincenzo.cesarano92@gmail.com>
2024-08-20 16:19:49 +0200

    up

---
+++ FRONT-END/UNIT3/Settimana1/D2-Esercizio/src/App.js
@@ -0,0 +1,18 @@
+import logo from "./logo.svg";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import CustomNavbar from "./components/CustomNavbar";
+import CustomFooter from "./components/CustomFooter";
+
+function App() {
+  return (
+    <>
+      <CustomNavbar />
+
+      <CustomFooter />
+    </>
+  );
+}
+
+export default App;
---
+++ FRONT-END/UNIT3/Settimana1/D2-Esercizio/src/components/CustomNavbar.jsx
@@ -0,0 +1,21 @@
+import { Navbar, Container, Nav } from 'react-bootstrap'
+
+const CustomNavbar = function () {
+  return (
+    <Navbar collapseOnSelect expand="md" bg="dark" data-bs-theme="dark">
+      <Container fluid>
+        <Navbar.Brand href="#home">EPIBOOKS</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Menu</Nav.Link>
+            <Nav.Link href="#">About</Nav.Link>
+            <Nav.Link href="#">Browse</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  )
+}
+
+export default CustomNavbar
