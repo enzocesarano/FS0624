376138688a1dbca7b4bc6dd982efc2e7b857bee4
Vincenzo Cesarano <vincenzo.cesarano92@gmail.com>
2024-08-22 16:36:38 +0200

    up

--- FRONT-END/UNIT3/Settimana1/D4-Esercizio/src/components/SingleBook.jsx
+++ FRONT-END/UNIT3/Settimana1/D4-Esercizio/src/components/SingleBook.jsx
@@ -1,6 +1,7 @@
 import { Component } from "react";
-
 import { Card, Col } from "react-bootstrap";
+
+import CommentArea from "./CommentArea";
 
 class SingleBook extends Component {
   state = {
@@ -14,20 +15,22 @@ class SingleBook extends Component {
     return (
       <Col
         xs={12}
-        md={2}
+        md={3}
         onClick={(e) => {
           this.setState({
             active: !this.state.active,
           });
         }}
       >
-        <Card className={`book-cover d-flex flex-column pointer ${active ? "border-3 border-danger" : ""}`}>
+        <Card className={`d-flex flex-column mb-2 pointer ${active ? "border-2 border-danger" : ""}`}>
           <Card.Img variant="top" src={book.img} />
           <Card.Body>
             <Card.Title>{book.title}</Card.Title>
           </Card.Body>
         </Card>
+        {active && <CommentArea elementId={book.asin}/>}
       </Col>
+      
     );
   }
 }
