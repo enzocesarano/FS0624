0161fcb34f5c7d8edf1928bbac77c60b171fac43
Vincenzo Cesarano <vincenzo.cesarano92@gmail.com>
2024-09-02 16:24:29 +0200

    up

---
+++ FRONT-END/UNIT3/Settimana3/D1-Esercizio/src/components/Job.jsx
@@ -0,0 +1,40 @@
+import { Row, Col } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { useDispatch, useSelector } from 'react-redux';
+
+const Job = ({ data }) => {
+
+  const arrayPref = useSelector((store) => {
+    return store.pref.arrayPref;
+  });
+
+  const selected = arrayPref.includes(data._id);
+
+  const dispatch = useDispatch();
+
+  return (
+    <Row
+      className="mx-0 mt-3 p-3"
+      style={{ border: "1px solid #00000033", borderRadius: 4 }}
+    >
+      <Col xs={3}>
+        <Link to={`/${data.company_name}`}>{data.company_name}</Link>
+      </Col>
+      <Col xs={3}>
+        <i className={selected ? "bi bi-heart-fill text-danger" : "bi bi-heart"} onClick={() => {
+          dispatch({
+            type: 'ADD_TO_PREF',
+            payload: data,
+          })
+        }}></i>
+      </Col>
+      <Col xs={6}>
+        <a href={data.url} target="_blank" rel="noreferrer">
+          {data.title}
+        </a>
+      </Col>
+    </Row>
+  );
+};
+
+export default Job;
