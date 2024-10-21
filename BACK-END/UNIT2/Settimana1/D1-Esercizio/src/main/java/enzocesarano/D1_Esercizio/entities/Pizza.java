c4e5feaf42fd84a0c4764161ef8c95a900f46a96
Vincenzo Cesarano <vincenzo.cesarano92@gmail.com>
2024-10-21 16:45:56 +0200

    D1-Esercizio

---
+++ BACK-END/UNIT2/Settimana1/D1-Esercizio/src/main/java/enzocesarano/D1_Esercizio/entities/Pizza.java
@@ -0,0 +1,23 @@
+package enzocesarano.D1_Esercizio.entities;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+public class Pizza extends Prodotto {
+    private List<Toppings> ingredienti = new ArrayList<>();
+
+    public Pizza(String name, double price, double carlories, List<Toppings> ingredienti) {
+        super(name, price, carlories);
+        this.ingredienti = ingredienti;
+    }
+
+}
