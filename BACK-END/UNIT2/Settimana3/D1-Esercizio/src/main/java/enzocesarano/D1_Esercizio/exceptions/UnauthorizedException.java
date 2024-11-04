f1c662f6c31dae747d7750842b9a3e07694c19e8
Vincenzo Cesarano <vincenzo.cesarano92@gmail.com>
2024-11-04 15:43:13 +0100

    d1-Esercizio

---
+++ BACK-END/UNIT2/Settimana3/D1-Esercizio/src/main/java/enzocesarano/D1_Esercizio/exceptions/UnauthorizedException.java
@@ -0,0 +1,7 @@
+package enzocesarano.D1_Esercizio.exceptions;
+
+public class UnauthorizedException extends RuntimeException {
+    public UnauthorizedException(String message) {
+        super(message);
+    }
+}
---
+++ BACK-END/UNIT2/Settimana3/D1-Esercizio/src/main/java/enzocesarano/D1_Esercizio/payloads/UserLoginDTO.java
@@ -0,0 +1,4 @@
+package enzocesarano.D1_Esercizio.payloads;
+
+public record UserLoginDTO(String id, String password) {
+}
